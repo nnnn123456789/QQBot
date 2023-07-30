bb50dc62ebfc7b7db50529617f9bc36cbcab0449
YUAN Z.M <yuan.zhao.ming@163.com>
2023-07-30 21:39:50 +0800

    更新了自动回复功能对换行的支持

--- autoreply.py
+++ autoreply.py
@@ -21,7 +21,7 @@ def get_autoans(msg, groupid = 0):
             if(time.time() - 600 >= t):
                 print( t)
                 i[2] = time.time()
-                return v
+                return v.replace('\\n', '\n')
     return ""
 
 #autoans_pool = [['招生办 电话', '北师大招生办电话：010-58807962', 0], ['保卫处 电话', '北师大保卫处24小时报警电话：010-58806110', 0], ['后勤 电话', '北师大后勤服务中心电话：010-58801111', 0], ['搜索引擎', '[CQ:image,file=fe828c62595577d364d46355216b9c60.image]', 0], ['校医院 电话', '北师大校医院电话：010-58805581，010-58808223', 0], ['图书馆 电话', '北师大图书馆电话：010-58809911', 0], ['节能办 电话', '北师大节能办电话：010-58808275', 0], ['师大地图', '师大高清校园地图见群文件', 0], ['学校 地图', '师大高清校园地图见群文件', 0], ['wei, zaima', '滚，不在', 0], ['教务电话本', '[CQ:image,file=26da040988c67e1aa8bf79d700e95e1a.image]', 0], ['麦当劳 电话', '北师大东门麦当劳电话：010-58802867', 0], ['？', '?', 0], ['小白鼠', '谁在叫 我？', 0]]
@@ -57,7 +57,7 @@ def add_auto_reply(args, groupid, qqid):
         return "权限不足"
     if not len(args) == 3:
         return "参数错误"
-    autoans_pool.append([args[1], args[2], 0])
+    autoans_pool.append([args[1].strip(), args[2].replace('\r\n', '\n').replace('\n', '\\n'), 0])
     write_to_file()
     return "添加成功"
     
