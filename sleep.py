2f1d8d31a32976c4ac5448a20501b3d665c78d90
YUAN Z.M <yuan.zhao.ming@163.com>
2023-08-03 22:03:57 +0800

    允许管理员sleep其他人

--- sleep.py
+++ sleep.py
@@ -1,6 +1,26 @@
 from api import *;
+from lib import *;
+from auth import *;
 
 
 def sleep(args, groupid, qqid):
     set_group_ban(qqid, groupid, 8*3600);
+    return ""
+
+
+
+def sleep_v2(args, groupid, qqid):
+    if not len(args) <= 2:
+        return "请求参数错误"
+    if len(args) == 1: 
+        aimqqid = qqid
+    else:
+        aimqqid = read_qqid(args[1]);
+    if aimqqid == qqid:
+        pass;
+    elif(get_authlevel(qqid, groupid)<5):
+        return "权限不足，请重试"
+    elif(get_authlevel(qqid, groupid)<=get_authlevel(aimqqid, groupid)):
+        return "权限不足，请重试"
+    set_group_ban(aimqqid, groupid, 8*3600);
     return ""
