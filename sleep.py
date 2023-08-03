7e64068c8db154b7b71cdf0358eeb33040caf557
YUAN Z.M <yuan.zhao.ming@163.com>
2023-08-03 22:11:18 +0800

    允许#sleep多人

--- sleep.py
+++ sleep.py
@@ -10,17 +10,14 @@ def sleep(args, groupid, qqid):
 
 
 def sleep_v2(args, groupid, qqid):
-    if not len(args) <= 2:
-        return "请求参数错误"
     if len(args) == 1: 
-        aimqqid = qqid
+        set_group_ban(qqid, groupid, 8*3600);
     else:
-        aimqqid = read_qqid(args[1]);
-    if aimqqid == qqid:
-        pass;
-    elif(get_authlevel(qqid, groupid)<5):
-        return "权限不足，请重试"
-    elif(get_authlevel(qqid, groupid)<=get_authlevel(aimqqid, groupid)):
-        return "权限不足，请重试"
-    set_group_ban(aimqqid, groupid, 8*3600);
+        for qq in args[1:]:
+            aimqqid = read_qqid(args[1]);
+            if(get_authlevel(qqid, groupid)<5):
+                return "权限不足，请重试"
+            elif(get_authlevel(qqid, groupid)<=get_authlevel(aimqqid, groupid)):
+                return "权限不足，请重试"
+            set_group_ban(aimqqid, groupid, 8*3600);
     return ""
